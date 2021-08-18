c11de41ddbdb46922e98085334b08c740f2d456e
Kushwaha-abhay <72202582+Kushwaha-abhay@users.noreply.github.com>
2021-08-18 12:15:35 +0530

    final

--- Backend/Controller/bookingController.js
+++ Backend/Controller/bookingController.js
@@ -2,7 +2,7 @@
 const stripe = require('stripe')('sk_test_51JPR10SAQv7kCWEsx2O4qgTHtZZLWbcFMxgmLHhfrhBFQkmlrXARzue8L7I2tPu7ZOnvhHkH9GqjCOP9TFRiPicr00wNiAMFQU')
 const planModel = require("../Models/plansModel");
 const userModel = require("../Models/usersModel");
-
+const bookingModel = require("../Models/bookingModel");
 
 async function createPaymentSesion(req,res){
     try{
@@ -36,7 +36,75 @@ async function createPaymentSesion(req,res){
     catch(error){
     console.log(error);
     }
-    }
+}
 
-    module.exports.createPaymentSesion = createPaymentSesion;
+async function createNewBooking(userEmail, planId) {
+  try{
+    const user = await userModel.findOne({email:userEmail});
+    const plan = await planModel.findById(planId);
+
+    const userId = user["_id"];
+
+    if(user.bookedPlanId == undefined){
+      const bookingOrder = {
+        userId : userId ,
+        bookedPlans : [ {planId:planId , name:plan.name  ,currentPrice : plan.price}    ]
+      }
+      const newBookingOrder = await bookingModel.create(bookingOrder);
+      user.bookedPlanId = newBookingOrder["_id"];
+      await user.save({validateBeforeSave:false});
+    }
+    else{
+      // already bought some plans !!!
+      const newBookedPlan = {
+        planId : planId,
+        name: plan.name,
+        currentPrice:plan.price
+      }
+      const userBookingObject = await bookingModel.findById(user.bookedPlanId);
+      userBookingObject.bookedPlans.push(newBookedPlan);
+      await userBookingObject.save();
+    }
+  }
+  catch(error){
+   return error;
+  }
+  // console.log("inside createNewBooking !!!");
+  // console.log(userEmail);
+  // console.log(planId);
+  // booking collection =>
+  // if(user.bookedPlanId){
+  // get booking id => go to booking document , push in bookedPlans
+  // }
+  // else
+  // new booking document
+  // id => user.bookedPlanId
+  // booking document.bookedPlans.push(obj);
+}
+async function checkoutComplete(req, res) {
+  try{
+    const END_POINT_KEY = process.env.END_POINT_KEY;
+    // console.log("Checkout complete ran !!");
+    // console.log("Request object");
+    // console.log(req);
+    const stripeSignature = req.headers["stripe-signature"];
+  
+    console.log("endpoint key = " , END_POINT_KEY);
+    console.log("stripeSign = " , stripeSignature);
+    console.log("Req.bdoy =>" , req.body);
+  
+    // if(req.body.data.type == "checkout.session.completed"){
+      const userEmail = req.body.data.object.customer_email;
+      const planId = req.body.data.object.client_reference_id;
+      await createNewBooking(userEmail , planId); 
+    // }
+  }
+  catch(error){
+    res.json({
+      error
+    })
+  }
+}
+
+module.exports.createPaymentSesion = createPaymentSesion;
     
